1356fe49eec56c1fd1d266fb377e22ea12c163cd
RolandYambao <rolandyambao@gmail.com>
2021-12-29 17:20:53 -0800

    Updated

---
+++ src/components/Quotes.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './Quotes.css';
+
+class Quotes extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Quotes;
---
+++ src/components/Store.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './Store.css';
+
+class Store extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Store;
