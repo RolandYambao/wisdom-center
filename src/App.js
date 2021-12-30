922885a4f8f19e184fffb212ec3f801dd0b35515
RolandYambao <rolandyambao@gmail.com>
2021-12-29 16:56:12 -0800

    Updated

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ import {
   Link,
 } from 'react-router-dom'
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Homepage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/quotes" element={<Quotes />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+// Components
+import Homepage from "./components/Homepage";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Store from "./components/Store";
+import Quotes from "./components/Quotes";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/quotes" element={<Quotes />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
